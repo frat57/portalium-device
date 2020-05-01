3c81bbbf20e0795e0e393fb0cab0d609ba9be344
frat57 <frat57571@hotmail.com>
2020-05-01 16:16:47 +0300

    back to changes

--- src/views/backend/default/index.php
+++ src/views/backend/default/index.php
@@ -1,37 +1,11 @@
 <?php
 
 use portalium\device\Module;
-use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\widgets\Pjax;
-/* @var $this yii\web\View */
-/* @var $searchModel backend\models\DeviceSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = Module::t('Device');
 ?>
 <div class="site-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php Pjax::begin(); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Device'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'food_id',
-            'user_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-    <?php Pjax::end(); ?>
+    <div class="jumbotron">
+        <h1><?= Module::t('Device Module - Backend') ?></h1>
+    </div>
 </div>
-
